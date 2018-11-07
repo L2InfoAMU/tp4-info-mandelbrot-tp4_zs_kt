23a402ddee5b3ca0949e2c12027fd6e13aeb8706
Teddy <teddy.khedoumi@etu.univ-amu.fr>
2018-11-07 11:34:20 +0100

    Inscription des membres
    Correction ToString

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -221,7 +221,7 @@ public class Complex {
     @Override
     public String toString() {
         return "Complex{" +
-                "real=" + imaginary +
+                "real=" + real +
                 ", imaginary=" + imaginary +
                 '}';
     }
